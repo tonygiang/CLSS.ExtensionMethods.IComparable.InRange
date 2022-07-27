743c9bed363e6a63ae183d8bc177b367b35560f7
tonygiang <tony_s_giang@yahoo.com>
2022-07-27 14:18:32 +0700

    Project push.

---
+++ IComparable.InRange.cs
@@ -0,0 +1,38 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+
+namespace CLSS
+{
+  public static partial class IComparableInRange
+  {
+    /// <summary>
+    /// Returns whether the source value is within the bound of
+    /// <paramref name="min"/> to <paramref name="max"/>.
+    /// </summary>
+    /// <typeparam name="T">The <see cref="IComparable{T}"/> type of the values
+    /// being compared.</typeparam>
+    /// <param name="value">The value being checked to be in a range.</param>
+    /// <param name="min">The lower bound to check
+    /// <paramref name="value"/> against. By default, <paramref name="min"/>
+    /// bound is inclusive.</param>
+    /// <param name="max">The exclusive upper bound to check
+    /// <paramref name="value"/> against. By default, <paramref name="max"/>
+    /// bound is exclusive.</param>
+    /// <param name="minInclusive">A value that specifies whether the range for
+    /// comparison is inclusive of <paramref name="min"/>.</param>
+    /// <param name="maxInclusive">A value that specifies whether the range for
+    /// comparison is inclusive of <paramref name="max"/>.</param>
+    /// <returns>Whether the source value is within the bound of
+    /// <paramref name="min"/> to <paramref name="max"/>.</returns>
+    public static bool InRange<T>(this T value, T min, T max,
+      bool minInclusive = true, bool maxInclusive = false)
+      where T : IComparable<T>
+    {
+      var minCompareRes = value.CompareTo(min);
+      var maxCompareRes = value.CompareTo(max);
+      return (minCompareRes > 0 || (minInclusive == (minCompareRes == 0)))
+        && (maxCompareRes < 0 || (maxInclusive == (maxCompareRes == 0)));
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# CLSS.ExtensionMethods.IComparable.InRange
+
+### Problem
+
+An expression to check if a value is within a certain range is somewhat long:
+
+```
+min <= value && value < max
+```
+
+Even longer when the type of your value does not implement comparison operators:
+
+```
+value.CompareTo(min) >= 0 && value.CompareTo(max) < 0
+```
+
+### Solution
+
+`InRange` does this in a more intuitive, shorter and functional style friendly way:
+
+```
+using CLSS;
+
+value.InRange(min, max)
+```
+
+No more short stops in your code reading to decode the meanings of comparison operators. When you read `InRange`, you know what it's doing.
+
+By default, `InRange` is min-inclusive and max-exclusive. To change this behavior, you can pass in a 3rd and 4th argument to specify min and max inclusivity:
+
+```
+using CLSS;
+
+// this is a min-exclusive and max-inclusive comparison
+value.InRange(min, max, false, true) // false if value == min, true if value == max
+```
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
